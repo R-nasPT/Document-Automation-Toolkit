bf4dbaa6211d5e1dd995c83fcf12afdd482d354f
Yotsatorn  Pathan <120444604+R-nasPT@users.noreply.github.com>
2024-07-05 16:37:17 +0700

    Update index.ts

--- react-to-print/src/documents/print/index.ts
+++ react-to-print/src/documents/print/index.ts
@@ -1,2 +1,2 @@
-export { default as ComponentToPrint } from "./component-print";
+export { default as ComponentToPrint } from "./component-print"; //<--ถ้าเอามารวมตรงนี้จะใช้ tailwind ไม่ได้
 export { default as PrintLabel } from "./print-label";
